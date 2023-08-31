44cabd6d411bc75ccced2fce647c8c9e5f2a7563
phattnguyen <n.phat@elsevier.com>
2023-08-31 14:44:57 +0700

    edit base url 5

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here 
     },
-    baseUrl: 'http://192.168.55.225:8080/'
+    baseUrl: 'http://192.168.55.225:9000/'
   },
 });
